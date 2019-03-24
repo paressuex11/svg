e2ae05f7051cc2d111c495d8b9de05ee2d543006
paressuex11 <1138514846@qq.com>
2019-03-24 17:45:10 +0800

    0

--- Project1/源.cpp
+++ Project1/源.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#define max(a, b) (a >= b ? a : b)
 using namespace std;
+const int view_x = 1100;
+const int view_y = 500;
+const int char_width = 10;
+const int char_lower_width = 7;
 
 void replace(string& str, const string& str1,const string& str2) {
 	size_t left = str.find(str1);
 	if (left == string::npos) {
-		cout << "not found";
 		return;
 	}
 	str.replace(left, str1.length(), str2);
@@ -38,6 +41,7 @@ public:
 	string value;
 	string label;
 	string word;
+	int node_length;
 	node* nodes[10];
 	int rank;
 	int child_number;
@@ -51,14 +55,20 @@ public:
 		this->rank = rank;
 		this->whether_have_child();
 		this->label = get_label(value);
+		replace(this->label, "<", "&lt");
+		replace(this->label, ">", "&gt");
 		this->whether_leaf = false;
 		if (!this->whether_dad) {
 			this->word = get_word(value);
 			this->whether_leaf = true;
+			replace(this->word, "<", "&lt");
+			replace(this->word, ">", "&gt");
 		}
 		else {
 			this->word = "";
 		}
+		this->node_length = max(this->label.length(), this->word.length());
+
 	}
 	void add_child(const string &child) {
 		node* children = new node(child, this->rank + 1);
@@ -85,24 +95,44 @@ public:
 		}
 	}
 	void build_svg() {
-		string rect = "<rect x = \"%x\" y = \"%y\" height = \"17\" fill-opacity=\"0.7\"  stroke-width=\"3\" stroke = \"#00FF00\" width = \"%z\" style = \"fill:url(#grlue)\" />";
+		string rect = "<rect x = \"%x\" y = \"%y\" height = \"17\" fill-opacity=\"0.9\"  stroke-width=\"3\" stroke = \"#00FF00\" width = \"%z\" style = \"fill:url(#grlue)\" />";
 		
-		replace(rect, "%x", to_string(this->x * 100) + "%");
-		replace(rect, "%y", to_string(this->y * 100) + "%");
-		replace(rect, "%z", to_string(this->label.length() * 9));
+		replace(rect, "%x", to_string(this->x ));
+		replace(rect, "%y", to_string(this->y ));
+		replace(rect, "%z", to_string(this->node_length * char_width));
+		string word = "";
+		int width = char_width;
+		if (this->word != "") {
+			word = "<rect x = \"%x\" y = \"%y\" height = \"17\" fill-opacity=\"0.9\"  stroke-width=\"3\" stroke = \"#00FF00\" width = \"%z\" style = \"fill:url(#grlue)\" />";
+			replace(word, "%x", to_string(this->x ) );
+			replace(word, "%y", to_string(this->y + 18));
+			replace(word, "%z", to_string(this->node_length * 10));
+			string text = "<text font-size=\"12\" x=\"%x\" y=\"%y\" fill=\"#000000\">" + this->word + "</text>";
+			for (char c : this->word) {
+				if (c <= 'z' && c >= 'a' || c == '\'' || c == ',') width = char_lower_width;
+				break;
+			}
+			replace(text, "%x", to_string(this->x + this->node_length*char_width / 2.0 - this->word.length()*width / 2.0));
+			replace(text, "%y", to_string(this->y + 33));
+			cout << word + text;
+		}
+		for (char c : this->label) {
+			if (c <= 'z' && c >= 'a' || c == '\'' || c == ',') width = char_lower_width;
+			break;
+		}
 		string text = "<text font-size=\"12\" x=\"%x\" y=\"%y\" fill=\"#000000\">" +this->label+  "</text>";
-		replace(text, "%x", to_string(this->x * 100 ) + "%");
-		replace(text, "%y", to_string(this->y * 100  + 17.0 / 7) + "%");
-		cout << rect + text;
+		replace(text, "%x", to_string(this->x + this->node_length*char_width / 2.0 - this->label.length()*width / 2.0) );
+		replace(text, "%y", to_string(this->y + 15));
+		cout << rect +  text;
 	}
 	void build_lines() {
 		string line = "<line stroke=\"#67C23A\" stroke-width=\"3\" stroke-opacity=\"0.6\" x1=\"%x1\" y1=\"%y1\" x2=\"%x2\" y2=\"%y2\"/>";
-		replace(line, "%x1", to_string(this->x * 100 + this->label.length() * 4.5 / 11.5) + "%");
-		replace(line, "%y1", to_string(this->y * 100 + 17.0 / 7) + "%");
+		replace(line, "%x1", to_string(this->x + this->node_length*char_width / 2.0) );
+		replace(line, "%y1", to_string(this->y ));
 		for (int i = 0; i < this->child_number; ++i) {
 			string linee = line;
-			replace(linee, "%x2", to_string(this->nodes[i]->x * 100) + "%");
-			replace(linee, "%y2", to_string(this->nodes[i]->y * 100 + 17.0 / 7) + "%");
+			replace(linee, "%x2", to_string(this->nodes[i]->x + this->nodes[i]->node_length*char_width / 2.0) );
+			replace(linee, "%y2", to_string(this->nodes[i]->y) );
 			cout << linee;
 		}
 	}
@@ -143,23 +173,28 @@ void build_vector(node* root, vector<node*>* leaves, vector<node*>* no_leaves) {
 void build_x()
 {
 	int max_rank = 0;
-	double offset = 0.000001;
-	double off = 1.0 / node::leaves.size();
-
-	while (off - offset  > 0.0005) {
-		offset += 0.0001;
-	}
+	int last_x = view_x - node::leaves[node::leaves.size() - 1]->node_length;
+	int sum = 0;
 	for (int i = 0; i < node::leaves.size(); ++i) {
-		node::leaves[i]->x = 1.0  * (i + 1) / node::leaves.size() - offset;
 		if (node::leaves[i]->rank > max_rank) max_rank = node::leaves[i]->rank;
+		sum += node::leaves[i]->node_length * char_width;
+	}
+	double space_len = (last_x - sum)* 1.0 / (node::leaves.size()-1);
+
+
+	for (int i = 0; i < node::leaves.size(); ++i) {
+		if (!i) node::leaves[i]->x = 0;
+		else {
+			node::leaves[i]->x = node::leaves[i - 1]->x + node::leaves[i - 1]->node_length * 10 + space_len;
+		}
 	}
 	for (int i = 0; i < node::leaves.size(); ++i) {
-		node::leaves[i]->y = 1.0  * node::leaves[i]->rank / max_rank - 0.1;
+		node::leaves[i]->y = 1.0  * node::leaves[i]->rank / max_rank * view_y;
 	}
 	while (!node::no_leaves.empty())
 	{
 		node* no_leaf = node::no_leaves[node::no_leaves.size() - 1];
-		no_leaf->y = 1.0 / (max_rank) * no_leaf->rank - 0.1;
+		no_leaf->y = 1.0 / (max_rank) * no_leaf->rank * view_y;
 		node::no_leaves.pop_back();
 		for (int i = 0; i < no_leaf->child_number; ++i) {
 			no_leaf->x += no_leaf->nodes[i]->x; 
@@ -184,7 +219,7 @@ void build_lines(node* root) {
 
 int main() {
 
-	string str = "(SBARQ (WHNP (WP What)) (SQ (VBZ is) (NP (NX (DT the) (JJ average) (NNS sales) (IN of) (DT the) (NNS journals) (WHNP (WDT that)) (NX (VBP have) (NP (DT an) (NN editor)) (WHADVP (WHNP (WP$ whose) (NX (NN work) (NN type))) (VBZ is) (`` ``)))) (NP (NNP Photo)) ('' ''))) (. ?))";
+	string str = "(SBARQ (WHNP (WP What)) (SQ (VBZ is) (NP (NX (DT the) (<JJ average) (NNS sales) (IN of) (DT the) (NNS journals) (WHNP (WDT that)) (NX (VBP have) (NP (DT an) (NN editor)) (WHADVP (WHNP (WP$ whose) (NX (NN work) (NN type))) (VBZ is) (`` ``)))) (NP (NNP Photo)) ('' ''))) (. ?))";
 	/*string str = "(S (S (VP (VB Show) (NP (DT all) (NX (NN card) (NN type) (NNS codes))))) (. .))";*/
 	node root(str, 1);
 	build_tree(&root);
